96f1015cc1dbff677d2a715732d04b3fdbfa0b07
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-06-18 01:22:02 +0200

    refactoring PipelineDrawTab -removed useless references

--- src/org/architecture_helper/GUI/PipelineDrawerTab.java
+++ src/org/architecture_helper/GUI/PipelineDrawerTab.java
@@ -4,22 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PipelineDrawerTab extends RunnableTab{
-	JLabel lblSource;
-	JLabel lblResult;
 	JTextArea taSourceCode;
 	JTextArea taResult;
 
 	public PipelineDrawerTab() {
-		lblSource = new JLabel("Source:");
-		lblResult = new JLabel("Result:");
 		taSourceCode = new JTextArea(20, 5);
 		taResult = new JTextArea(20, 5);
 
 		setLayout(new GridLayout(5, 1));
 
-		add(lblSource);
+		add(new JLabel("Source:"));
 		add(taSourceCode);
-		add(lblResult);
+		add(new JLabel("Result:"));
 		add(taResult);
 	}
 
