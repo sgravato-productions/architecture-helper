6140b7a1f03be84e571734c3d4a9263851c1d351
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-06-18 02:23:05 +0200

    PipelineDrawer graphical rework -removed all the old layout +now uses various BorderLayouts and a JSplitPane -removed default dimentions from JTextArea +JTextArea are now Scrollable

--- src/org/architecture_helper/GUI/PipelineDrawerTab.java
+++ src/org/architecture_helper/GUI/PipelineDrawerTab.java
@@ -1,26 +1,62 @@
 package org.architecture_helper.gui;
 
+import org.architecture_helper.backend.Instruction;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PipelineDrawerTab extends RunnableTab{
+	//region
 	JTextArea taSourceCode;
 	JTextArea taResult;
 
+	private final Font textAreasFont = new Font("monospaced", Font.PLAIN, 12);
+	//endregion
+
 	public PipelineDrawerTab() {
-		taSourceCode = new JTextArea(20, 5);
-		taResult = new JTextArea(20, 5);
+		setLayout(new BorderLayout());
 
-		setLayout(new GridLayout(5, 1));
+		//region creating top panel
+		JPanel topPanel = new JPanel(new BorderLayout());
 
-		add(new JLabel("Source:"));
-		add(taSourceCode);
-		add(new JLabel("Result:"));
-		add(taResult);
+		topPanel.add(new JLabel("Source:"), BorderLayout.NORTH);
+
+		taSourceCode = new JTextArea();
+		taSourceCode.setFont(textAreasFont);
+		topPanel.add(new JScrollPane(taSourceCode), BorderLayout.CENTER);
+		//endregion
+
+		//region creating bottom panel
+		JPanel botPanel = new JPanel(new BorderLayout());
+
+		botPanel.add(new JLabel("Result:"), BorderLayout.NORTH);
+
+		taResult = new JTextArea();
+		taResult.setFont(textAreasFont);
+		botPanel.add(new JScrollPane(taResult), BorderLayout.CENTER);
+		//endregion
+
+		//region creating split pane
+		JSplitPane topBotSplit = new JSplitPane(JSplitPane.VERTICAL_SPLIT, topPanel, botPanel);
+		add(topBotSplit, BorderLayout.CENTER);
+
+		//setting split pane to 0.5
+		topBotSplit.setResizeWeight(0.5);
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				topBotSplit.setDividerLocation(0.5);
+			}
+		});
+		//endregion
+
 	}
 
 	@Override
 	public void run() {
-		System.out.println("STARTING PIPELINE DRAW");
+
 	}
 }
